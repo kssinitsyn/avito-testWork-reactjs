6ad593754d69dea30ec3e557a991fd42cf0818c7
Kirill Sinitsyn <k.s.sinitsyn@gmail.com>
2020-01-20 22:49:19 +0300

    Добавлены два компонента, компонент для рендера самой карточки, и компонент для рендера списка карточек, пока даннные статичные, роутинг так же не настроил

---
+++ src/components/card.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "../style.css"
+
+const Card = (props) => {
+    return (
+        <li key={props.item.id} id={props.item.id} className="card__item">
+            <a className="card__link" href="#">
+                <img className="card__image" src={props.item.previewImage} alt={props.item.title}/>
+                <div className="card__text-wrapper">
+                    <h2 className="card__title"> {props.item.title} </h2>
+                    <p className="card__address"> {props.item.address} </p>
+                    <p className="card__price"> {props.item.price} </p>
+                </div>
+            </a>
+        </li>
+    )
+}
+
+export default Card;
---
+++ src/components/cardList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './card';
+import '../style.css';
+
+const CardList = (props) => {
+  return (
+    <ul className="card__list">
+      {props.props.map((item, index) => (
+        <Card key={index} item={item} /> 
+      ))}
+    </ul>
+  )
+}
+
+export default CardList;
