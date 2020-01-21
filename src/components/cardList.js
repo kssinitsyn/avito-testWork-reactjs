648fcb3cb97cb7beeee5f6848af90bd8dc4b30fc
Kirill Sinitsyn <k.s.sinitsyn@gmail.com>
2020-01-21 17:53:58 +0300

    Настроил фетчинг, теперь данные тянутся с адреса, рендер корректный, роутинг работает, но до конца не настроен

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -2,15 +2,45 @@ import React from 'react';
 import Card from './card';
 import '../style.css';
 
+export default class CardList extends React.Component {
+  constructor(props) {
+    super(props)
 
-const CardList = (props) => {
-  return (
-    <ul className="card__list">
-      {props.props.map((item, index) => (
-        <Card key={index} item={item} /> 
-      ))}
-    </ul>
-  )
-}
+    this.state = {
+      data: [],
+      isFetching: true,
+      error: null
+    };
+  }
 
-export default CardList;
+    componentDidMount() {
+      fetch('http://134.209.138.34/items')
+        .then(res => res.json())
+        .then(res => this.setState({
+          data: res,
+          isFetching: false,
+        }))
+        .catch(e => {
+          this.setState({
+            isFetching: false,
+            error: e
+        })
+        })
+    }
+
+    render() {
+      const {data, isFetching, error } = this.state;
+
+      if(isFetching) {
+        return <div>Loading...</div>
+      }
+
+      return (
+        <ul className="card__list">
+          {data.map((item, index) => (
+            <Card key={index} item={item} /> 
+          ))}
+        </ul>
+      )
+    }
+  }
